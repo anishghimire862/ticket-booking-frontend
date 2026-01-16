b0dd94926e278a0f437d66a5be9b4740e2882205
Anish Ghimire <anishghimire862@gmail.com>
2026-01-16 18:38:26 +0545

    chore: configure Axios with VITE_API_BASE_URL

---
+++ src/api/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 5000,
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API error:', error)
+    return Promise.reject(error)
+  },
+)
+
+export default api
